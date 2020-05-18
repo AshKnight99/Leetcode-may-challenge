be102be7b7fd42da0ce39af814a18ff724190ff6
Ashknight99 <rachanaraut1999@gmail.com>
2020-05-18 23:37:56 +0530

    Day 18 commit

---
+++ Day 18.java
@@ -0,0 +1,27 @@
+/*
+Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. In other words, one of the first string's permutations is the substring of the second string.
+Example 1:
+Input: s1 = "ab" s2 = "eidbaooo"
+Output: True
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+Input:s1= "ab" s2 = "eidboaoo"
+Output: False
+*/
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+        int[] map = new int[26];
+        for(char c : s1.toCharArray()) map[c - 'a']++;
+        int j = 0, i = 0;
+        int count_chars = s1.length();
+        while(j < s2.length()){
+            if(map[s2.charAt(j++) - 'a']-- > 0)
+                count_chars--;
+            if(count_chars == 0) return true;
+            if(j - i == s1.length() && map[s2.charAt(i++) - 'a']++ >= 0)
+                count_chars++;
+        }
+        return false;
+    }
+}
